54989a84fbd1480e5159a536df697f87a5c50236
Om Nigam <om2021064@akgec.ac.in>
2024-07-04 00:10:37 +0530

    adding home page

---
+++ components/Filters.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const links = ["all", "Next 13", "frontend", "backend", "fullStack"];
+
+const Filters = () => {
+  return <ul className="">{}</ul>;
+};
+
+export default Filters;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,6 +9,27 @@ const Navbar = () => {
         <Link href="/">
           <Image src="/jsm-logo.svg" alt="logo" width={55} height={40} />
         </Link>
+
+        <Image
+          src="/hamburger-menu.svg"
+          width={30}
+          height={30}
+          alt="Hamburger menu"
+          className="block md:hidden"
+        />
+
+        <ul className="flex-center gap-x-3 max-md:hidden md:gap-x-10">
+          <li className="body-text text-gradient_blue-purple !font-bold">
+            <Link href="https://jsmastery.pro/next13" target="_blank">
+              Next.js 13.4 Course
+            </Link>
+          </li>
+          <li className="body-text  !font-bold">
+            <Link href="https://jsmastery.pro/masterclass" target="_blank">
+              Master Class
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
